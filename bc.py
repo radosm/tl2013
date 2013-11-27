d850ac0b86835fbcf9bee8ab9982fd19ddf5b7e8
kevin.piotrkowski <kevin.piotrkowski@avature.net>
2013-11-27 01:02:50 -0300

    Actualizacion de bc.py para que utilice el nuevo modulo de config

--- bc.py
+++ bc.py
@@ -5,19 +5,17 @@
 # version simplificada del Super Collider.
 # -----------------------------------------------------------------------------
 
-import debug
+import config
 from debug import log
 from bcGrammar import *
-
-debug.habilitado = debug.SIMPLE
 
 log('Generando la entrada')
 #valores = range(8,15)
 #entrada = ';'.join(map(lambda x: str(x), valores)) + '.play'
 
 entrada = 'sin(2.1,1.8).play;silence.play(3)'
-#entrada = '{3+21;4}-{2;1};5'
+entrada = '{2;3;4;8}*{1;2;3};{{4;8;6}/2}'
 log('Haciendo el parsing')
-resultado = yacc.parse(entrada, debug = debug.habilitado == debug.FULL)
+resultado = yacc.parse(entrada, debug = config.DEBUG == config.DEBUG_FULL)
 
 log('Fin')
